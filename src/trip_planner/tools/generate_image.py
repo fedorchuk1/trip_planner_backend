f4e8a28b108b1da85a9bb15a4d61c1387640c0ab
Illia Strelnykov <ilya.strelnikov@youscan.io>
2025-07-07 17:22:44 +0300

    add image generation, add API key to secrets.yaml, add dotenv for easier local notebook development

---
+++ src/trip_planner/tools/generate_image.py
@@ -0,0 +1,34 @@
+import os
+import requests
+
+url = "https://api.getimg.ai/v1/flux-schnell/text-to-image"
+BEARER_KEY = os.environ.get("GETIMG_API_KEY_ILLIA")
+
+
+def generate_image(
+    image_prompt: str,
+    height: int = 512,
+    width: int = 1024,
+    steps: int = 4,
+    output_format: str = "jpeg",
+    response_format: str = "b64"
+) -> str:
+    payload = {
+        "prompt": image_prompt,
+        "height": height,
+        "width": width,
+        "steps": steps,
+        "output_format": output_format,
+        "response_format": response_format,
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {BEARER_KEY}"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    return response.json()["image"]
+
+
